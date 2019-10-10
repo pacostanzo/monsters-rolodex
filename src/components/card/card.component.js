1aca9fcd9154a66848fe7e2489d7404e98d0eb5f
Pablo Costanzo <costanzopa@gmail.com>
2019-10-09 21:34:47 -0300

    Adding cardlist and card components.

---
+++ src/components/card/card.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './card.styles.css'
+
+
+export const Card = (props) => (
+    <div className='card-container'>
+        <h1>{props.monster.name}</h1>
+    </div>
+);
