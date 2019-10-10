7d0a1eacdfd4718fc0c692da6573749a38268125
Pablo Costanzo <costanzopa@gmail.com>
2019-10-09 22:33:49 -0300

    Adding cardlist and card components.

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -5,6 +5,9 @@ import './card.styles.css'
 
 export const Card = (props) => (
     <div className='card-container'>
-        <h1>{props.monster.name}</h1>
+        <img src={`https://robohash.org/${props.monster.id}/?set=set2&size=180x180`}
+             alt='monster'/>
+        <h2>{props.monster.name}</h2>
+        <p>{props.monster.email}</p>
     </div>
 );
