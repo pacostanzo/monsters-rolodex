657ec62d3154a2b09071308b49a7edec92c51b2b
costanzopa <costanzopa@gmail.com>
2019-10-11 01:14:47 -0300

    Updates

---
+++ precache-manifest.cf14d156b6afd65714e71c5f57d01487.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15930e159d71baa97fada6df2c37778a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2bdd5b50dec5111f872e",
+    "url": "/monsters-rolodex/static/css/main.606b96f2.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "2bdd5b50dec5111f872e",
+    "url": "/monsters-rolodex/static/js/main.65a35dd9.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
